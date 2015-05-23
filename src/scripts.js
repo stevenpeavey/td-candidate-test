cae0b68a3b2fa3fc40f6e6231b074310612385e5
Steven Peavey <steven@westlakedesign.com>
2015-05-23 16:39:13 -0400

    initial add

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,35 @@
 (function () {
-	console.log('Hello, world!');
+
+	$('.message').hover(function(){
+		$('.inbox-message-reply').removeClass('disabled').addClass('btn-primary');
+	},function(){
+		$('.inbox-message-reply').addClass('disabled').removeClass('btn-primary');
+	});
+
+
+	$('.inbox-message-reply').click(function(){
+		$('.inbox-message-reply').hide();
+		$('.message-send').removeClass('hidden');
+	});
+
+	$('.inbox-message-send-reply').click(triggerModalAnimation);
+
+	function triggerModalAnimation() {
+    //console.log('yo');
+    $('#myModal').addClass('in');
+		$(".send-message-progress").animate({
+	    width:'100%'
+	  }, 2000, function() {
+	    // Animation complete.
+	    setTimeout(function(){
+		    $(".progress, .modal-header").hide();
+		    $('.message-send-success').removeClass('hide');
+	  	}, 1000);
+	  });
+
+	  $('.close-message').click(function(){
+	  	location.reload(); 
+	  });
+	};
+
 })();
